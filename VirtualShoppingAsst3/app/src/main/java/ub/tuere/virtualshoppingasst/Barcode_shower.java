63f2d21ab2fcc1d40bc76ddaef750f8bd6b670ca
ak29 <ak229@buffalo.edu>
2016-11-06 00:20:31 -0400

    Initial Commit

---
+++ VirtualShoppingAsst3/app/src/main/java/ub/tuere/virtualshoppingasst/Barcode_shower.java
@@ -0,0 +1,13 @@
+package ub.tuere.virtualshoppingasst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Barcode_shower extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_shower);
+    }
+}
